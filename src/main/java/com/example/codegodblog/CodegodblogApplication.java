0887ad2cbdb5fcb2da25be936269234eec7523c4
smebkoo70 <645979671@qq.com>
2022-11-18 00:41:22 +0800

    2022 11 18 00:40

---
+++ src/main/java/com/example/codegodblog/CodegodblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.codegodblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegodblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodegodblogApplication.class, args);
+    }
+
+}
